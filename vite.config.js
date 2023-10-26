904adf6094fa962b4b82af3a7c06c151f0924e21
lewmas9152 <smnjuguna22@gmail.com>
2023-10-26 09:24:44 +0300

    changed position of base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base: "/portfolio"
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio",
   plugins: [react()],
 })
